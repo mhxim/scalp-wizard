ce8d214894c23c79c41dc286d2b940d8073ee5f8
MHuesler <hueslermaxim@gmail.com>
2024-02-05 10:53:10 +0100

    minor imports

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-# docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ppo/#ppopy
-import copy
+# Following cleanRL baseline
 import datetime
 import os
 import random
 import time
-from dataclasses import dataclass
-
 import gymnasium as gym
+import tyro
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import tyro
 from torch.distributions.categorical import Categorical
 from torch.utils.tensorboard import SummaryWriter
-
-from market_simulation.market_env import MarketEnvironment
 import torch.nn.functional as F
+from market_simulation.market_env import MarketEnvironment
+from dataclasses import dataclass
 from torch.distributions import Normal
 
 @dataclass
