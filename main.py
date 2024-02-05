c997d1a2fa974778b5af60e2f8bdbb547a12de45
MHuesler <hueslermaxim@gmail.com>
2024-02-05 10:52:16 +0100

    minor

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class Args:
     """if toggled, cuda will be enabled by default"""
     track: bool = False
     """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
+    wandb_project_name: str = "scalp_wizard"
     """the wandb's project name"""
     wandb_entity: str = None
     """the entity (team) of wandb's project"""
